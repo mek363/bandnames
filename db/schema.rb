39ff55c0db172b3bc626b79e6cdae8bfa36144e5
mek363 <mat@vyperhelp.com>
2019-12-28 13:48:00 -0600

    Add admin flag and user delete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191223225746) do
+ActiveRecord::Schema.define(version: 20191228192735) do
 
   create_table "bandnames", force: :cascade do |t|
     t.string "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20191223225746) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
